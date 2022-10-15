4acae42d1bd88991c5825f6b3285821e4b2c77a5
Riyaan75 <riyaan75.RH@gmail.com>
2022-10-16 00:18:25 +0530

    env back

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const mongo = require("mongodb");
-//const uri = process.env.MONGO_DB_URI
-const uri="mongodb+srv://riyaan75:ayshaRH75@cluster0.0emyic0.mongodb.net/jobOpening"
+const uri = process.env.MONGO_DB_URI
 console.log(uri)
 async function connect() {
   let client;
